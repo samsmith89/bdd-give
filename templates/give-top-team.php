efffbffa7b263f79c99eb2c74c560e8a1602a175
Sam Smith <samcbr600@gmail.com>
2020-01-28 09:38:32 -0500

    pulled the correct tema link

--- templates/give-top-team.php
+++ templates/give-top-team.php
@@ -41,7 +41,9 @@ function top_give_team_forms_function($atts)
                 <th>Total</th>
                 <th>Link to Donate</th>
                 <?php
-                        while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                        while ($wp_query->have_posts()) : $wp_query->the_post();
+                        $team_link = get_permalink();
+                        ?>
 
                     <!-- start of the row -->
                     <tr class="<?php post_class(); ?>">
@@ -55,7 +57,7 @@ function top_give_team_forms_function($atts)
                             $team_name   = get_field('team_name');
                             $args = array(
                                 'post_type'         => 'give_forms',
-                                'posts_per_page'    => 3,
+                                'posts_per_page'    => 2,
                                 'meta_query' => array(
                                     array(
                                         'key'     => 'team_name',
@@ -80,12 +82,10 @@ function top_give_team_forms_function($atts)
 
                             $team_goal_final = array_sum($team_goal);
                             echo $team_goal_final;
-                            wp_reset_postdata();
-
                          ?>
                         </td>
                         <!-- Team donation form link -->
-                        <td><a href="<?php echo the_permalink(); ?>" class="readmore give-donation-form-link"><?php _e('Donate Now', 'give'); ?> &raquo;</a></td>
+                        <td><a href="<?php echo $team_link ?>" class="readmore give-donation-form-link"><?php _e('Donate Now', 'give'); ?> &raquo;</a></td>
                     </tr>
                 <?php
                         endwhile;
